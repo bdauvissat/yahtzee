128ed73288087bace05d24e5208a27f3d115ab72
Benjamin Dauvissat <bdauvissat@gmail.com>
2017-08-19 22:44:08 +0200

    Initialisation des TU

---
+++ src/test/java/com/coding/dojo/yahtzee/PossibleScores.java
@@ -0,0 +1,81 @@
+package com.coding.dojo.yahtzee;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.assertj.core.api.Fail.fail;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PossibleScores {
+
+    @Test
+    public void listeUn() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void listeDeux() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void listeTrois() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void listeQuatre() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void listeCinq() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void listeSix() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void brelan() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void carre() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void full() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void petiteSuite() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void grandeSuite() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void Yathzee() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void primeYathzee() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void chance() {
+        fail("Not yet implemented");
+    }
+}
